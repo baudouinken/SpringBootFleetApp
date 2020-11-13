b4d4e6139e0f392cd82433d49e592c7ab616ad42
baudouin <baudouin>
2020-11-14 00:51:38 +0100

    comment ItBase

--- src/test/java/com/fleet/project/ItBase.java
+++ src/test/java/com/fleet/project/ItBase.java
@@ -1,20 +1,20 @@
-package com.fleet.project;
-
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.fleet.project.services.CountryService;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class ItBase {
-
-	@Autowired
-	CountryService countryService;
-
-}
+//package com.fleet.project;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.fleet.project.services.CountryService;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@TestPropertySource(locations = "classpath:application-test.properties")
+//public class ItBase {
+//
+//	@Autowired
+//	CountryService countryService;
+//
+//}
